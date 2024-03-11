54f230a8798c82aeeffc12ffa62a0a220b2fab3b
LocTM <tranminhloc.anhk@gmail.com>
2024-03-11 15:14:23 +0700

    add ICustomerReposiroty

--- src/main/java/com/codegym/service/CustomerService.java
+++ src/main/java/com/codegym/service/CustomerService.java
@@ -1,29 +1,35 @@
 package com.codegym.service;
 
 import com.codegym.model.Customer;
+import com.codegym.repository.ICustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-public class CustomerService implements ICustomerService {
+@Service
+public class CustomerService implements ICustomerService{
+
+    @Autowired
+    private ICustomerRepository iCustomerRepository;
 
     @Override
     public Iterable<Customer> findAll() {
-        return null;
+        return iCustomerRepository.findAll();
     }
 
     @Override
     public Optional<Customer> findById(Long id) {
-        return Optional.empty();
+        return iCustomerRepository.findById(id);
     }
 
     @Override
     public Customer save(Customer customer) {
-        return null;
+        return iCustomerRepository.save(customer);
     }
 
     @Override
     public void remove(Long id) {
-
+        iCustomerRepository.deleteById(id);
     }
 }
